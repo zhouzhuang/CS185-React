cab7fcbafd74cab2dd7ce6ffcec549d757014e1a
Zhou Zhuang <zhouzhuang@ucsb.edu>
2020-04-26 10:47:56 -0700

    Updates

---
+++ precache-manifest.554b5657ab6dec93bb61e4c62645045a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f06f8305eb1f56a406ecccb5264c87",
+    "url": "/CS185-react/index.html"
+  },
+  {
+    "revision": "d83c69c3ba46c244031e",
+    "url": "/CS185-react/static/css/main.4b312d31.chunk.css"
+  },
+  {
+    "revision": "7e99d5ae375ae0fe086c",
+    "url": "/CS185-react/static/js/2.9c79f3d2.chunk.js"
+  },
+  {
+    "revision": "61ba7d8397432dd9b09a150a080df22b",
+    "url": "/CS185-react/static/js/2.9c79f3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83c69c3ba46c244031e",
+    "url": "/CS185-react/static/js/main.677bf673.chunk.js"
+  },
+  {
+    "revision": "b42d95b8376c0fee38b1",
+    "url": "/CS185-react/static/js/runtime-main.b399b4d4.js"
+  },
+  {
+    "revision": "57751155af4b82c8df14413368282910",
+    "url": "/CS185-react/static/media/RatCal.57751155.jfif"
+  },
+  {
+    "revision": "3b60c463a00c00e6f85d7fa6e3e5e530",
+    "url": "/CS185-react/static/media/coral.3b60c463.jfif"
+  },
+  {
+    "revision": "9f93b190ea87d0c2d8cf1a96fa1fb39d",
+    "url": "/CS185-react/static/media/eqSearch.9f93b190.jfif"
+  },
+  {
+    "revision": "971eed9000baa2333e4ee8a492be07c8",
+    "url": "/CS185-react/static/media/img1.971eed90.jpg"
+  },
+  {
+    "revision": "e0f1eb10da0e0ef173471c83f5e74a53",
+    "url": "/CS185-react/static/media/img2.e0f1eb10.jpg"
+  },
+  {
+    "revision": "2829f9699f799339963a1106e4506779",
+    "url": "/CS185-react/static/media/img3.2829f969.jpg"
+  },
+  {
+    "revision": "ddf561fecfc84010691781d6633153a8",
+    "url": "/CS185-react/static/media/img4.ddf561fe.jpg"
+  },
+  {
+    "revision": "3f6ce15fbc5b9c683b1d2cca33ff620d",
+    "url": "/CS185-react/static/media/img5.3f6ce15f.jpg"
+  },
+  {
+    "revision": "534d7eaec54a40fb8e319c8b3be69fd3",
+    "url": "/CS185-react/static/media/img6.534d7eae.jpg"
+  },
+  {
+    "revision": "278420b58de589b6f23f13936a3e853a",
+    "url": "/CS185-react/static/media/img7.278420b5.jpg"
+  },
+  {
+    "revision": "631e09a0324db44c11bad85b2fec70bf",
+    "url": "/CS185-react/static/media/img8.631e09a0.jpg"
+  },
+  {
+    "revision": "7f20debf12790822798ac8a6960e2a76",
+    "url": "/CS185-react/static/media/img9.7f20debf.jpg"
+  },
+  {
+    "revision": "00da78c6917070f42268ffbe4d3b1117",
+    "url": "/CS185-react/static/media/pistachios.00da78c6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40c875cdb97176e5a5815cebef94e67e.js"
+  "/CS185-react/precache-manifest.554b5657ab6dec93bb61e4c62645045a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CS185-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
